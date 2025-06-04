e6ff3a743f0d400d7431cd9d5744302d036262fa
Shima Shahraeini <shmshahraeini@gmail.com>
2025-06-04 18:36:19 +0330

    Add files via upload

---
+++ Session3-ComputerVision/extractConcreteFeatures.m
@@ -0,0 +1,24 @@
+function concreteTable = extractConcreteFeatures(ds)
+
+intensityAvg = [];
+intensitySTD = [];
+imgName = [];
+
+while hasdata(ds)
+
+    [img,info] = read(ds);
+    
+    img = im2double(img);
+    img = im2gray(img);
+    
+    intensityAvg = [intensityAvg; mean(img(:))];
+    intensitySTD = [intensitySTD; std(img(:))];
+    
+    [~,name,ext] = fileparts(info.Filename);
+    imgName = [imgName; string(name) + string(ext)];
+
+end
+
+label = categorical(ds.Labels);
+concreteTable = table(label,imgName,intensityAvg,intensitySTD);
+end
